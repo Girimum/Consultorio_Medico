8ca0772d3e08355cfeb3147c24d1a418d7df0666
mac <mac@192.168.23.104>
2017-03-22 11:17:32 -0300

    Teste de classe Prof. Claudio

---
+++ Consultorio_medico_2_0/src/consultorio_medico_2_0/TesteClaudio.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package consultorio_medico_2_0;
+
+/**
+ *
+ * @author mac
+ */
+public class TesteClaudio {
+    //Testando 123... kkk
+}
