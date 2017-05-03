fcc6124d66c233c6671ec3e87c12db9398a5c85a
Girimun <Girimun@20.20.0.17>
2017-05-03 16:50:37 -0300

    Criação de novas interfaces

--- Consultorio_medico_2_0/src/conexao/conexao.java
+++ Consultorio_medico_2_0/src/conexao/conexao.java
@@ -1,6 +1,9 @@
 
 package conexao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class conexao {
-    
+    Connection conexao = DriverManager.getConnection("jdbc:mysql://127.0.0.1/banco","root","admin");
 }
